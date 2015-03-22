8d7380236f793c7b3b3cbcc83f735f101daaaf67
Austin Smith <austin@westlakedesign.com>
2015-03-22 08:42:43 -0700

    1.4 revisions

--- hamsters.js
+++ hamsters.js
@@ -543,7 +543,7 @@ hamsters._runtime.wakeUp = function() {
 			}
 			hamster.postMessage(food, bufferarray);
 		} else { //Legacy Fallback..much slower
-			hamster.postMessage(JSONfood);
+			hamster.postMessage(food);
 		}
 	};
 
