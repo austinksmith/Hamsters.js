60951775f22ec91e064466b770400789f0b4a35c
Austin Smith <austin@westlakedesign.com>
2015-09-09 09:28:35 -0400

    Tests

---
+++ tests/spec/HamstersSpec.js
@@ -0,0 +1,63 @@
+describe("WebHamsters Core Functionality", function() {
+
+
+  it("Hamsters.wheel.legacy should be boolean", function() {
+    expect(hamsters.wheel.legacy).toMatch(/true|false/);
+  });
+
+  it("Hamsters.tools.aggregate should aggregate array of subarrays", function() {
+    expect(hamsters.tools.aggregate([[1],[2]])).toEqual([1,2]);
+  });
+
+  it("Hamsters.tools.splitArray should split array into subarrays", function() {
+    expect(hamsters.tools.splitArray([1,2], 2)).toEqual([[1],[2]]);
+  });
+
+  it("Hamsters.wheel.newTask should create new task", function() {
+    var task = hamsters.wheel.newTask(0, 1, null, 'int32', "rtn.data.push(params.array)", null);
+    expect(hamsters.wheel.tasks[task.id]).not.toBeNull();
+  });
+
+  it("Hamsters.wheel.trackInput should track thread input", function() {
+    var task = hamsters.wheel.newTask(0, 1, null, 'int32', "rtn.data.push(params.array)", null);
+    var input = hamsters.wheel.trackInput([], 1, hamsters.wheel.tasks[0], {num: 32});
+    expect(hamsters.wheel.tasks[task.id].input).not.toBeNull();
+  });
+
+  describe("WebHamsters running asynchronously", function() {
+    beforeEach(function(done) {
+      done();
+    });
+    var dataTypes = ['Int8','Int16','Int32','Float32','Float64','Uint16','Uint32','Uint8'];
+    for (var i = dataTypes.length - 1; i >= 0; i--) {
+      it("Computes 7th Fibonacci Number ("+dataTypes[i]+")", function(done) {
+        hamsters.run({num: 7}, function() {
+          var fib = function(n) {
+            if (n < 2){
+              return 1;
+            } else {
+              return fib(n-2) + fib(n-1);
+            }
+          }
+          rtn.data.push(fib(params.num-1));
+        }, function(res) {
+          result = res[0];
+          expect(result).toEqual(13);
+          done();
+        }, 1, true, dataTypes[i]);
+      });
+    }
+
+    for (var i = dataTypes.length - 1; i >= 0; i--) {
+      it("Calculates square root of 4000 ("+dataTypes[i]+")", function(done) {
+        hamsters.run({num: 4000}, function() {
+          rtn.data.push(Math.floor(Math.sqrt(params.num)));
+        }, function(res) {
+          result = res[0];
+          expect(result).toEqual(63);
+          done();
+        }, 1, true, dataTypes[i]);
+      });
+    }
+  });
+});
