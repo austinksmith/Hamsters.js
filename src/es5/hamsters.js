10263f95e02c4edff0811c3ae2391004fd51cc79
Austin Smith <austin@asmithdev.com>
2017-02-19 12:00:15 -0500

    Scope global.self within node

--- src/es5/hamsters.js
+++ src/es5/hamsters.js
@@ -64,7 +64,7 @@ var hamsters = {
     hamsters.wheel.env.node = typeof process === "object" && typeof require === "function" && !hamsters.wheel.env.browser && !hamsters.wheel.env.worker && !hamsters.wheel.env.reactNative;
     hamsters.wheel.env.reactNative = !hamsters.wheel.env.node && typeof global === 'object';
     hamsters.wheel.env.shell = !hamsters.wheel.env.browser && !hamsters.wheel.env.node && !hamsters.wheel.env.worker && !hamsters.wheel.env.reactNative;
-    if(hamsters.wheel.env.reactNative) {
+    if(hamsters.wheel.env.reactNative || hamsters.wheel.env.node) {
       global.self = global;
     }
     if(hamsters.wheel.env.node) {
