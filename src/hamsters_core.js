0b29154f76eabd70de53d2f2fc40e25b348cc08a
Austin <austin@asmithdev.com>
2017-05-03 19:04:52 -0400

    Read me, rename files

--- src/hamsters_core.js
+++ src/hamsters_core.js
@@ -28,29 +28,29 @@ exports.hamsters = {
 
     function setupBrowserSupport() {
       if(!Worker || ["Kindle/3.0", "Mobile/8F190", "IEMobile"].indexOf(navigator.userAgent) !== -1) {
-        hamsters.wheel.env.legacy = true;
+        this.wheel.env.legacy = true;
       }
       if(navigator.userAgent.toLowerCase().indexOf("firefox") !== -1) {
-        hamsters.maxThreads = (hamsters.maxThreads > 20 ? 20 : hamsters.maxThreads);
+        this.maxThreads = (this.maxThreads > 20 ? 20 : this.maxThreads);
       }
       if(isIE(10)) {
         try {
-          let hamster = new Worker("src/common/hamsters.wheel.min.js");
+          let hamster = new Worker("src/common/this.wheel.min.js");
           hamster.terminate();
-          hamsters.wheel.env.ie10 = true;
+          this.wheel.env.ie10 = true;
         } catch(e) {
-          hamsters.wheel.env.legacy = true;
+          this.wheel.env.legacy = true;
         }
       }
     }
 
     function setupWorkerSupport() {
       try {
-        hamsters.wheel.uri = URL.createObjectURL(createBlob("(" + String(giveHamsterWork()) + "());"));
-        const SharedHamster = new SharedWorker(hamsters.wheel.uri, "SharedHamsterWheel");
+        this.wheel.uri = URL.createObjectURL(createBlob("(" + String(giveHamsterWork()) + "());"));
+        const SharedHamster = new SharedWorker(this.wheel.uri, "SharedHamsterWheel");
         SharedHamster.terminate();
       } catch(e) {
-        hamsters.wheel.env.legacy = true;
+        this.wheel.env.legacy = true;
       }
     }
 
@@ -64,49 +64,49 @@ exports.hamsters = {
     }
 
     function setupHamstersEnvironment(onSuccess) {
-      hamsters.wheel.env.browser = typeof window === "object";
-      hamsters.wheel.env.worker  = typeof importScripts === "function";
-      hamsters.wheel.env.node = typeof process === "object" && typeof require === "function" && !hamsters.wheel.env.browser && !hamsters.wheel.env.worker && !hamsters.wheel.env.reactNative;
-      hamsters.wheel.env.reactNative = !hamsters.wheel.env.node && typeof global === "object";
-      hamsters.wheel.env.shell = !hamsters.wheel.env.browser && !hamsters.wheel.env.node && !hamsters.wheel.env.worker && !hamsters.wheel.env.reactNative;
+      this.wheel.env.browser = typeof window === "object";
+      this.wheel.env.worker  = typeof importScripts === "function";
+      this.wheel.env.node = typeof process === "object" && typeof require === "function" && !this.wheel.env.browser && !this.wheel.env.worker && !this.wheel.env.reactNative;
+      this.wheel.env.reactNative = !this.wheel.env.node && typeof global === "object";
+      this.wheel.env.shell = !this.wheel.env.browser && !this.wheel.env.node && !this.wheel.env.worker && !this.wheel.env.reactNative;
       if(typeof navigator !== "undefined") {
-        hamsters.maxThreads = navigator.hardwareConcurrency;
+        this.maxThreads = navigator.hardwareConcurrency;
       }
       if(typeof startOptions !== "undefined") {
         processStartOptions();
       }
-      if(hamsters.wheel.env.browser) {
+      if(this.wheel.env.browser) {
         setupBrowserSupport();
       }
-      if(hamsters.wheel.env.worker) {
+      if(this.wheel.env.worker) {
         setupWorkerSupport();
       }
-      if(hamsters.wheel.env.reactNative || hamsters.wheel.env.node) {
+      if(this.wheel.env.reactNative || this.wheel.env.node) {
         global.self = global;
       }
-      if(hamsters.wheel.env.shell || typeof Worker === "undefined") {
-        hamsters.wheel.env.legacy = true;
+      if(this.wheel.env.shell || typeof Worker === "undefined") {
+        this.wheel.env.legacy = true;
       }
       if(typeof Uint8Array === "undefined") {
-        hamsters.wheel.env.transferrable = false;
+        this.wheel.env.transferrable = false;
       }
       onSuccess();
     }
 
     function spawnHamsters() {
       if(typeof URL !== "undefined") {
-        hamsters.wheel.uri = URL.createObjectURL(createBlob("(" + String(giveHamsterWork()) + ")();"));
+        this.wheel.uri = URL.createObjectURL(createBlob("(" + String(giveHamsterWork()) + ")();"));
       }
-      if(hamsters.persistence) {
-        let i = hamsters.maxThreads;
+      if(this.persistence) {
+        let i = this.maxThreads;
         for (i; i > 0; i--) {
-          hamsters.wheel.hamsters.push(hamsters.wheel.newHamster());
+          this.wheel.this.push(this.wheel.newHamster());
         }
       }
     }
 
     function giveHamsterWork() {
-      if(hamsters.wheel.env.worker) {
+      if(this.wheel.env.worker) {
         return workerWorker;
       }
       return worker;
@@ -193,18 +193,18 @@ exports.hamsters = {
     }
 
     function legacyHamsterWheel(inputArray, hamsterfood, aggregate, callback, task, threadid, hamster, memoize) {
-      hamsters.wheel.trackThread(task, hamsters.wheel.queue.running, threadid);
-      if(memoize || hamsters.debug) {
-        hamsters.wheel.trackInput(inputArray, threadid, task, hamsterfood);
+      this.wheel.trackThread(task, this.wheel.queue.running, threadid);
+      if(memoize || this.debug) {
+        this.wheel.trackInput(inputArray, threadid, task, hamsterfood);
       }
-      hamsters.wheel.legacyProcessor((hamsterfood, inputArray, output) => {
-        hamsters.wheel.clean(task, threadid);
+      this.wheel.legacyProcessor((hamsterfood, inputArray, output) => {
+        this.wheel.clean(task, threadid);
         task.output[threadid] = output.data;
         if(task.workers.length === 0 && task.count === task.threads) {
-          callback(hamsters.wheel.getOutput(task.output, aggregate, output.dataType));
-          hamsters.wheel.tasks[task.id] = null;
-          if(hamsters.cache && memoize !== false) {
-            hamsters.wheel.memoize(task.fn, task.input, output.data, output.dataType);
+          callback(this.wheel.getOutput(task.output, aggregate, output.dataType));
+          this.wheel.tasks[task.id] = null;
+          if(this.cache && memoize !== false) {
+            this.wheel.memoize(task.fn, task.input, output.data, output.dataType);
           }
         }
       });
@@ -212,40 +212,40 @@ exports.hamsters = {
     }
 
     function hamsterWheel(inputArray, hamsterfood, aggregate, callback, task, threadid, hamster, memoize) {
-      if(hamsters.maxThreads === hamsters.wheel.queue.running.length) {
-        hamsters.wheel.poolThread(inputArray, hamsterfood, threadid, callback, task, aggregate, memoize);
+      if(this.maxThreads === this.wheel.queue.running.length) {
+        this.wheel.poolThread(inputArray, hamsterfood, threadid, callback, task, aggregate, memoize);
         return;
       }
-      if(memoize || hamsters.debug) {
-        hamsters.wheel.trackInput(inputArray, threadid, task, hamsterfood);
+      if(memoize || this.debug) {
+        this.wheel.trackInput(inputArray, threadid, task, hamsterfood);
       }
       if(!hamster) {
-        if(hamsters.persistence) {
-          hamster = hamsters.wheel.hamsters[hamsters.wheel.queue.running.length];
+        if(this.persistence) {
+          hamster = this.wheel.hamsters[this.wheel.queue.running.length];
         } else {
-          hamster = hamsters.wheel.newHamster();
+          hamster = this.wheel.newHamster();
         }
       }
-      hamsters.wheel.trainHamster(threadid, aggregate, callback, task, hamster, memoize);
-      hamsters.wheel.trackThread(task, hamsters.wheel.queue.running, threadid);
+      this.wheel.trainHamster(threadid, aggregate, callback, task, hamster, memoize);
+      this.wheel.trackThread(task, this.wheel.queue.running, threadid);
       hamsterfood.array = inputArray;
-      hamsters.wheel.feedHamster(hamster, hamsterfood);
+      this.wheel.feedHamster(hamster, hamsterfood);
       task.count += 1; //Increment count, thread is running
-      if(hamsters.debug === "verbose") {
+      if(this.debug === "verbose") {
         console.info("Spawning Hamster #" + threadid + " @ " + new Date().getTime());
       }
     }
 
     function chewGarbage() {
-      delete hamsters.init;
+      delete this.init;
       startOptions = null;
     }
 
     setupHamstersEnvironment(() => {
-      if(hamsters.wheel.env.legacy) {
-        hamsters.wheel.newWheel = legacyHamsterWheel;
+      if(this.wheel.env.legacy) {
+        this.wheel.newWheel = legacyHamsterWheel;
       } else {
-        hamsters.wheel.newWheel = hamsterWheel;
+        this.wheel.newWheel = hamsterWheel;
         spawnHamsters();
       }
       chewGarbage();
