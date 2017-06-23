004a407578e3c387681d5d5549cedb9fd915d5fc
austinksmith <austin@asmithdev.com>
2017-06-23 12:48:00 -0400

    Spawning threads

--- src/core/hamsters.js
+++ src/core/hamsters.js
@@ -13,6 +13,9 @@ const hamsterWheel = require("./wheel/hamster-wheel");
 const hamsterTools = require("./tools/hamster-tools");
 const memoizer = require("./cache/memoizer");
 const threadPool = require("./pool/thread-pool");
+const createBlob = require("./wheel/data/create-blob");
+const giveHamsterWork = require("./processor/hamster-worker");
+
 
 
 const processStartOptions = (startOptions) => {
@@ -36,12 +39,12 @@ const wakeHamsters = () => {
     var i = hamsterEnvironment.maxThreads;
     for (i; i > 0; i--) {
       if(hamsterEnvironment.ie10) {
-        threadPool.hamsters.push(new Worker('src/common/wheel.min.js'));
+        threadPool.activeThreads.push(new Worker('src/common/wheel.min.js'));
       }
       if(hamsterEnvironment.worker) {
-        threadPool.hamsters.push(new SharedWorker(hamsterEnvironment.uri, 'SharedHamsterWheel'));
+        threadPool.activeThreads.push(new SharedWorker(hamsterEnvironment.uri, 'SharedHamsterWheel'));
       } else {
-        threadPool.hamsters.push(new Worker(hamsterEnvironment.uri));
+        threadPool.activeThreads.push(new Worker(hamsterEnvironment.uri));
       }
     }
   }
@@ -49,18 +52,17 @@ const wakeHamsters = () => {
 
   function setupBrowserSupport() {
     if(!Worker || ['Kindle/3.0', 'Mobile/8F190', 'IEMobile'].indexOf(navigator.userAgent) !== -1) {
-      hamsters.wheel.env.legacy = true;
+      hamsterEnvironment.legacy = true;
     }
     if(navigator.userAgent.toLowerCase().indexOf('firefox') !== -1) {
       hamsters.maxThreads = (hamsters.maxThreads > 20 ? 20 : hamsters.maxThreads);
     }
-    if(isIE(10)) {
+    if(hamsterEnvironment.ie10) {
       try {
         var hamster = new Worker('src/common/wheel.min.js');
         hamster.terminate();
-        hamsters.wheel.env.ie10 = true;
       } catch(e) {
-        hamsters.wheel.env.legacy = true;
+        hamsterEnvironment.legacy = true;
       }
     }
   }
@@ -70,67 +72,61 @@ const wakeHamsters = () => {
       hamsters.wheel.uri = URL.createObjectURL(createBlob('(' + String(giveHamsterWork()) + '());'));
       var SharedHamster = new SharedWorker(hamsters.wheel.uri, 'SharedHamsterWheel');
     } catch(e) {
-      hamsters.wheel.env.legacy = true;
+      hamsterEnvironment.legacy = true;
     }
   }
 
-  function setupHamstersEnvironment(onSuccess) {
-    hamsters.wheel.env.browser = typeof window === "object";
-    hamsters.wheel.env.worker  = typeof importScripts === "function";
-    hamsters.wheel.env.node = typeof process === "object" && typeof require === "function" && !hamsters.wheel.env.browser && !hamsters.wheel.env.worker && !hamsters.wheel.env.reactNative;
-    hamsters.wheel.env.reactNative = !hamsters.wheel.env.node && typeof global === 'object';
-    hamsters.wheel.env.shell = !hamsters.wheel.env.browser && !hamsters.wheel.env.node && !hamsters.wheel.env.worker && !hamsters.wheel.env.reactNative;
-    if(typeof navigator !== 'undefined' && typeof navigator.hardwareConcurrency !== 'undefined') {
-      hamsters.maxThreads = navigator.hardwareConcurrency;
+  function setupHamstersEnvironment(startOptions) {
+    if(typeof startOptions !== "undefined") {
+      processStartOptions(startOptions);
     }
-    if(typeof startOptions !== 'undefined') {
-      processStartOptions();
-    }
-    if(hamsters.wheel.env.browser && !hamsters.wheel.env.reactNative) {
+    if(hamsterEnvironment.browser && !hamsterEnvironment.reactNative) {
       setupBrowserSupport();
     }
-    if(hamsters.wheel.env.worker) {
+    if(hamsterEnvironment.worker) {
       setupWorkerSupport();
     }
-    if(hamsters.wheel.env.node || hamsters.wheel.env.reactNative) {
+    if(hamsterEnvironment.node || hamsterEnvironment.reactNative) {
       global.self = global;
       if(typeof hamsters.Worker !== 'undefined') {
         global.Worker = hamsters.Worker;
       }
     }
-    if(hamsters.wheel.env.shell || typeof Worker === 'undefined') {
-      hamsters.wheel.env.legacy = true;
+    if(hamsterEnvironment.shell || typeof Worker === 'undefined') {
+      hamsterEnvironment.legacy = true;
     }
     if(typeof Uint8Array === 'undefined') {
-      hamsters.wheel.env.transferrable = false;
+      hamsterEnvironment.transferrable = false;
     }
     if(typeof SharedArrayBuffer !== 'undefined') {
-      hamsters.wheel.env.atomics = true;
+      hamsterEnvironment.atomics = true;
     }
-    onSuccess();
+    if(!hamsterEnvironment.legacy) {
+      spawnHamsters();
+    }
   }
 
   function spawnHamsters() {
-    if(hamsters.wheel.env.browser) {
-      hamsters.wheel.uri = URL.createObjectURL(createBlob('(' + String(giveHamsterWork()) + ')();'));
+    if(hamsterEnvironment.browser) {
+      hamsterEnvironment.uri = URL.createObjectURL(createBlob('(' + String(giveHamsterWork()) + ')();'));
     }
-    if(hamsters.persistence) {
-      var i = hamsters.maxThreads;
+    if(true) {
+      var i = hamsterEnvironment.maxThreads;
       for (i; i > 0; i--) {
-        if(hamsters.wheel.env.ie10) {
-          hamsters.wheel.hamsters.push(new Worker('./common/wheel.min.js'));
-        } else if(hamsters.wheel.env.node) {
-          hamsters.wheel.hamsters.push(new Worker(giveHamsterWork()));
-        } else if(hamsters.wheel.env.worker) {
-          hamsters.wheel.hamsters.push(new SharedWorker(hamsters.wheel.uri, 'SharedHamsterWheel'));
+        if(hamsterEnvironment.ie10) {
+          threadPool.activeThreads.push(new Worker('./common/wheel.min.js'));
+        } else if(hamsterEnvironment.node && !hamsterEnvironment.browser) {
+          threadPool.activeThreads.push(new Worker(giveHamsterWork()));
+        } else if(hamsterEnvironment.worker) {
+          threadPool.activeThreads.push(new SharedWorker(hamsters.wheel.uri, 'SharedHamsterWheel'));
         } else {
-          hamsters.wheel.hamsters.push(new Worker(hamsters.wheel.uri));
+          threadPool.activeThreads.push(new Worker(hamsterEnvironment.uri));
         }
       }
     }
   }
 
-global.hamsters = {
+self.hamsters = {
   version: "4.2.0",
   persistence: true,
   habitat: hamsterEnvironment,
@@ -138,12 +134,7 @@ global.hamsters = {
   tools: hamsterTools,
   errors: [],
   init: (startOptions) => {
-    if(typeof startOptions !== "undefined") {
-      processStartOptions(startOptions);
-    }
-    if(hamsterEnvironment.legacy) {
-
-    }
+    setupHamstersEnvironment(startOptions);
   },
   run: executeTask
 };
