f73e283c23ca8455b86d8be249c86c2b6d886511
Austin Smith <austin@asmithdev.com>
2017-08-11 15:33:54 -0400

    Decouple library

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,18 +2,16 @@
 
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var babel = require('gulp-babel');
 
 gulp.task('build', function() {
-  return gulp.src(['src/core/*'])
-    .pipe(concat('hamsters.min.js'))
+  return gulp.src(['src/core/main.js'])
+    .pipe(concat('hamsters.js'))
     .pipe(babel())
-    .pipe(browserify())
-    // .pipe(uglify())
-    .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-    .pipe(gulp.dest('build'));
+    .on('error', function (err) {
+    		gutil.log(gutil.colors.red('[Error]'), err.toString());
+    	}
+    ).pipe(gulp.dest('build'));
 })
