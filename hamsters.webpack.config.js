9315e18b6ac225bb741975816158a4b2a5319fa6
Austin Smith <austin@asmithdev.com>
2017-12-03 09:07:05 -0500

    Remove more extra dependencies, leaner build config

--- hamsters.webpack.config.js
+++ hamsters.webpack.config.js
@@ -1,11 +1,7 @@
-require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 const webpackSettings = require('./webpack.config');
 
 const optimizingPlugins = [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify('production'),
-  }),
   new webpack.optimize.OccurrenceOrderPlugin,
   new webpack.optimize.UglifyJsPlugin,
 ];
