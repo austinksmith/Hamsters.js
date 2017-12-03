1608b57bda476ffc0057d0e0ff4a1679d3826d8c
Austin Smith <austin@asmithdev.com>
2017-12-03 08:56:24 -0500

    Minified and uglified output, remove useless dependencies

--- hamsters.webpack.config.js
+++ hamsters.webpack.config.js
@@ -2,17 +2,14 @@ require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 const webpackSettings = require('./webpack.config');
 
-// const optimizingPlugins = [
-//   new webpack.DefinePlugin({
-//     'process.env.NODE_ENV': JSON.stringify('production'),
-//   }),
-//   new webpack.optimize.OccurrenceOrderPlugin,
-//   // new webpack.optimize.UglifyJsPlugin,
-// ];
+const optimizingPlugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  }),
+  new webpack.optimize.OccurrenceOrderPlugin,
+  new webpack.optimize.UglifyJsPlugin,
+];
 
-// webpackSettings.plugins = optimizingPlugins;
-// webpackSettings.entry = webpackSettings.entry.filter((entryName) => {
-//   return (entryName.indexOf('hot/dev-server') === -1);
-// });
+webpackSettings.plugins = optimizingPlugins;
 
 module.exports = webpackSettings;
