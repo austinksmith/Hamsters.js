4670906ee9d16084303f3d64b49e2928c73e4e2b
Austin Smith <austin@asmithdev.com>
2017-12-07 08:32:44 -0500

    WIP Add promises, refactor library internally to use promises

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
+
+var browser = {
+  target: 'web',
   devtool: 'sourcemap',
   entry: [
     './src/hamsters',
   ],
   output: {
     path: path.resolve('./build'),
-    filename: 'hamsters.min.js',
+    filename: 'hamsters.web.min.js',
     library: 'hamsters',
     libraryTarget: 'var'
   },
@@ -25,3 +27,34 @@ module.exports = {
     ],
   }
 };
+
+var server = {
+  target: 'node',
+  devtool: 'sourcemap',
+  entry: [
+    './src/hamsters',
+  ],
+  output: {
+    path: path.resolve('./build'),
+    filename: 'hamsters.node.min.js',
+    library: 'hamsters',
+    libraryTarget: 'var'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.js?$/],
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+        },
+      }
+    ],
+  }
+};
+
+module.exports = [
+  browser, 
+  server 
+];
