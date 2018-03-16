85977764e86fc1c24d2e4129d2f2e23e9fb98fc2
Austin Smith <austin@asmithdev.com>
2018-03-16 09:14:11 -0400

    Update hamstersWheel file to match regular scaffold logic

--- build/common/hamstersWheel.js
+++ build/common/hamstersWheel.js
@@ -10,65 +10,67 @@
 ***********************************************************************************/
 
 (function)() {
-  'use strict';
 
-  var params = {};
-  var rtn = {};
+   'use strict';
 
-  function setDefaults(incomingMessage) {
-    params = incomingMessage.data;
-    rtn = {
-      data: [],
-      dataType: params.dataType ? params.dataType.toLowerCase() : null,
-      threadStart: Date.now()
-    };
-  };
-
-  function prepareReturn(returnObject) {
-    var dataType = returnObject.dataType;
-    if(dataType) {
-      returnObject.data = typedArrayFromBuffer(dataType, returnObject.data);
+    if(typeof self === 'undefined') {
+      let self = (global || window || this);
     }
-    return returnObject;
-  };
 
-  function typedArrayFromBuffer(dataType, buffer) {
-    var types = {
-      'uint32': Uint32Array,
-      'uint16': Uint16Array,
-      'uint8': Uint8Array,
-      'uint8clamped': Uint8ClampedArray,
-      'int32': Int32Array,
-      'int16': Int16Array,
-      'int8': Int8Array,
-      'float32': Float32Array,
-      'float64': Float64Array
-    };
-    if (!types[dataType]) {
-      return buffer;
+    self.params = {};
+    self.rtn = {};
+
+    function prepareReturn(returnObject) {
+      let dataType = returnObject.dataType;
+      if(dataType) {
+        returnObject.data = typedArrayFromBuffer(dataType, returnObject.data);
+      }
+      return returnObject;
     }
-    return new types[dataType](buffer);
-  };
 
-  function prepareTransferBuffers(hamsterFood) {
-    var buffers = [];
-    var key = null;
-    for (key in hamsterFood) {
-      if (hamsterFood.hasOwnProperty(key) && hamsterFood[key]) {
-        if(hamsterFood[key].buffer) {
-          buffers.push(hamsterFood[key].buffer);
-        } else if(Array.isArray(hamsterFood[key]) && typeof ArrayBuffer !== 'undefined') {
-          buffers.push(new ArrayBuffer(hamsterFood[key]));
+    function typedArrayFromBuffer(dataType, buffer) {
+      const types = {
+        'uint32': Uint32Array,
+        'uint16': Uint16Array,
+        'uint8': Uint8Array,
+        'uint8clamped': Uint8ClampedArray,
+        'int32': Int32Array,
+        'int16': Int16Array,
+        'int8': Int8Array,
+        'float32': Float32Array,
+        'float64': Float64Array
+      };
+      if (!types[dataType]) {
+        return buffer;
+      }
+      return new types[dataType](buffer);
+    }
+
+    function prepareTransferBuffers(hamsterFood) {
+      let buffers = [];
+      let key = null;
+      for (key in hamsterFood) {
+        if (hamsterFood.hasOwnProperty(key) && hamsterFood[key]) {
+          if(hamsterFood[key].buffer) {
+            buffers.push(hamsterFood[key].buffer);
+          } else if(Array.isArray(hamsterFood[key]) && typeof ArrayBuffer !== 'undefined') {
+            buffers.push(new ArrayBuffer(hamsterFood[key]));
+          }
         }
       }
+      return buffers;
     }
-    return buffers;
-  };
 
-  function onmessage(incomingMessage) {
-    setDefaults(incomingMessage);
-    new Function(params.fn)();
-    rtn.threadEnd = Date.now();
-    postMessage(prepareReturn(rtn), prepareTransferBuffers(rtn));
-  };
+    addEventListener('message', function(incomingMessage) {
+      params = incomingMessage.data;
+      rtn = {
+        data: [],
+        dataType: (params.dataType ? params.dataType.toLowerCase() : null),
+        threadStart: Date.now()
+      };
+      new Function(params.hamstersJob)();
+      rtn.threadEnd = Date.now();
+      postMessage(prepareReturn(rtn), prepareTransferBuffers(rtn));
+    });
+
 })();
